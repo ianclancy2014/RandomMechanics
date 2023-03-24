38ade492fecee235a8f2ea698922a253bb2f2d8c
ianclancy2014 <61367982+ianclancy2014@users.noreply.github.com>
2023-03-24 16:31:31 -0500

    update 2
    Tweaked the beginning of the code a bit and experimented with a different way to determine who goes first. Old code commented out. Still needs a ton of work but it's getting there.

--- ThirdBroGameboy.js
+++ ThirdBroGameboy.js
@@ -12,25 +12,47 @@ let oldestBrothersTurn = 0;
 let secondBrothersTurn = 0;
 let youngestBrothersTurn = 0;
 
-let dadScheduled = true;
-let oldestScheduled = true;
-let secondScheduled = true;
-let youngestScheduled = true;
+let father = true;
+let first = true;
+let second = true;
+let third = true;
+
+let willContinue = true;
 
 let i = 0;
 
-while (i < 100) {
-  let turnToUseGameboy = Math.floor(Math.random() * 4);
-  if (turnToUseGameboy == 0) {
-    fathersTurn = fathersTurn + 1;
-  } else if (turnToUseGameboy == 1) {
-    oldestBrothersTurn = oldestBrothersTurn + 1;
-  } else if (turnToUseGameboy == 2) {
-    secondBrothersTurn = secondBrothersTurn + 1;
-  } else {
-    youngestBrothersTurn = youngestBrothersTurn + 1;
+// while (i < 14) {
+//   let turnToUseGameboy = Math.floor(Math.random() * 4); //decides who the next player will be
+//   if (turnToUseGameboy <= 3 && ) {
+//     fathersTurn = fathersTurn + 1;
+//   } else if (turnToUseGameboy == 1) {
+//     oldestBrothersTurn = oldestBrothersTurn + 1;
+//   } else if (turnToUseGameboy == 2) {
+//     secondBrothersTurn = secondBrothersTurn + 1;
+//   } else {
+//     youngestBrothersTurn = youngestBrothersTurn + 1;
+//   }
+//   i = i + 1;
+//   let oldTurn = turnToUseGameboy;
+//   willContinueToPlay();
+// }
+
+function willContinueToPlay(placeholder) {
+  let z = Math.floor(Math.random() * 2);
+  if (z == 0) {
+    placeholder = false;
+    console.log(placeholder);
+  } else if (z == 1) {
+    placeholder = true;
+    console.log(placeholder);
   }
-  i = i + 1;
+}
+
+function whoWantsToPlay() {
+  willContinueToPlay(father);
+  willContinueToPlay(first);
+  willContinueToPlay(second);
+  willContinueToPlay(third);
 }
 
 // while (i < 100) {
@@ -46,6 +68,8 @@ while (i < 100) {
 //   }
 //   i = i + 1;
 // }
+
+console.log(whoWantsToPlay());
 
 console.log(
   fathersTurn,
